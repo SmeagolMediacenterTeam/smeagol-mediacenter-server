386e5205036b5ede65f2168e5a9a70da6f4709a7
Damien Duboeuf <smeagolworms4@gmail.com>
2015-12-17 18:47:43 +0100

    Add cache minimal

--- config.js
+++ config.js
@@ -1,6 +1,18 @@
-var _global = typeof window !== 'undefined' ? window : global;
+(function () {
+	var _global = typeof window !== 'undefined' ? window : global;
+	var Path    = require('path');
 
-_global.GollumJS = {
-	config: {
-	}
-};
+	_global.GollumJS = {
+		config: {
+
+			node: {
+				run_path: Path.resolve("."),
+				tmp_path: '%node.run_path%/tmp'
+			},
+
+			cache: {
+				path: '%node.tmp_path%/cache',
+			}
+		}
+	};
+})();
