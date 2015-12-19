fa91c4a0deb058521ca3b3d96c22c495a334db1c
SmeagolWorms4 <smeagolworms4@gmail.com>
2015-12-19 17:44:20 +0100

    ZTCaller:  - Add only one response in cache  - Fix multi call request from ZT page with proxy. The proxy group multi request with only

--- src/Server/Scheduler.js
+++ src/Server/Scheduler.js
@@ -11,36 +11,37 @@ GollumJS.NS(Server, function() {
 			
 		},
 
-		push: function () {
+		push: function (step) {
 			var _this = this;
 			return new Promise(function(resolve, reject) {
 
 				_this._pile.push({
 					resolve: resolve,
-					reject: reject ,
-					next: function (error) {
-						if (_this._pile.length) {
-							_this._step();
-						} else {
-							_this._running = false;
-						}
-					},
-					cb: function () {
-						console.log ("cool");
-						step();
-					}
+					reject: reject,
+					cb: step
 				});
-				if (_this._running) {
-					_this._step();
+				
+				if (!_this._running) {
+					_this._exec();
 				}
 
 			});
 		},
 
-		_step: function() {
+		_exec: function() {
+			
 			this._running = true;
+						
+			var _this = this;
 			var schedule = this._pile.shift();
-			schedule.cb(schedule.resolve, schedule.reject, schedule.next);
+			
+			schedule.cb(schedule.resolve, schedule.reject, function (error) {
+				if (_this._pile.length) {
+					_this._exec();
+				} else {
+					_this._running = false;
+				}
+			});
 		}
 
 	});
