8051efcc811f7074fedd35a9804b6bbd1413856b
Damien Duboeuf <smeagolworms4@gmail.com>
2016-01-06 18:01:49 +0100

    Add component view picture

---
+++ public/components/media/view-picture/ViewPicture.js
@@ -0,0 +1,22 @@
+App.Component.Element.SMC = App.Component.Element.SMC || {};
+App.Component.Element.SMC.Main = App.Component.Element.SMC.Main || {};
+
+GollumJS.NS(App.Component.Element.SMC.Main, function() {
+
+	this.ViewPicture = new GollumJS.Class({
+		
+		Extends: App.Component.Element,
+
+		init: function () {
+		},
+
+		beforeRender: function (done) {
+			done();
+		},
+
+		afterDisplay: function() {
+		}
+		
+	});
+
+});
